1863681b5a05ffcfd6c1eab85454bb7107dc56e0
Mark Hainline <wucheeman@gmail.com>
2018-04-26 09:21:04 -0400

    initial commit of .js files

---
+++ letter.js
@@ -0,0 +1,5 @@
+/*
+
+**HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console)
+
+*/

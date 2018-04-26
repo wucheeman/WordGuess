c228edc61bcc835b9d001815c76cad6c0b8507e1
Mark Hainline <wucheeman@gmail.com>
2018-04-26 16:10:23 -0400

    getting last save into repo :(

--- letter.js
+++ letter.js
@@ -1,8 +1,4 @@
-/*
 
-**HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console)
-
-*/
 const Letter = function(letter) {
   this.letter = letter;
   this.placeholder = '_';
@@ -20,20 +16,3 @@ const Letter = function(letter) {
   };
 }
 module.exports = Letter;
-
-// function Letter(letter) {
-//   this.letter = letter;
-//   this.placeholder = '_';
-//   this.guessed = false;
-//   this.getLetter = function() {
-//     if (this.guessed) {
-//       return this.letter;
-//     }
-//     return this.placeholder;
-//   };
-//   this.checkGuess = function(guess) {
-//     if (this.letter === guess) {
-//       this.guessed = true;
-//     }
-//   };
-// }

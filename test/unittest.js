aab5fe509d8b6ae8b8886ae487f873efb17a47b9
Mark Hainline <wucheeman@gmail.com>
2018-04-27 14:20:54 -0400

    completed Word.makeLetterArray() and unittest

--- test/unittest.js
+++ test/unittest.js
@@ -28,7 +28,15 @@ console.log("Expected: a ; actual: " + letter.getDisplayLetter() + '\n');
 //==========================================================================
 
 const Word = require('../word.js');
-console.log(typeof Word);
 
-const word = new Word('cat');
-console.log(word);
+const mysteryWord = new Word ('cat');
+console.log(mysteryWord);
+console.log('');
+console.log('Calling mysteryWord.makeLetterArray');
+mysteryWord.makeLetterArray();
+console.log('Accessing mysteryWord.letterArray');
+console.log("Expected: c, a, t; actual: " +
+             mysteryWord.letterArray[0].letter + ', ' +
+             mysteryWord.letterArray[1].letter + ', ' +
+             mysteryWord.letterArray[2].letter );
+console.log('');
--- word.js
+++ word.js
@@ -1,3 +1,6 @@
+// @ts-check
+// This turns on type checking in VS Code
+
 /*
 **HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: https://jsbin.com/facawetume/edit?js,console)
 */
@@ -6,11 +9,15 @@ const Letter = require('./letter.js');
 
 const Word = function(word) {
   this.word = word; // not sure if this is needed
-  this.makeLetterArray = function(word) {
+  this.letterArray = [];
+  this.makeLetterArray = function() {
     // creates array of Letter objects matching word
+    for (let letter of this.word) {
+      let letterObj = new Letter(letter);
+      this.letterArray.push(letterObj);
+    }
   };
-  this.letterArray = this.makeLetterArray(word);
-  this.getWord = function() {
+    this.getWord = function() {
     // calls each letter object's getDisplayLetter() and concatenates the response.
   };
   this.displayStateOfGame = function() {

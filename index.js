b3c184ca6b113ead51e72ab897509d3a720f41c4
Mark Hainline <wucheeman@gmail.com>
2018-04-29 17:59:01 -0400

    initial dialog for user input

--- index.js
+++ index.js
@@ -2,15 +2,66 @@
 // This turns on type checking in VS Code
 
 // require inpuirer
+const inquirer = require('inquirer');
 // require Word
+// npm install random-words - package generating a random word
 
 // Create dictionary
+// TODO: extend dictionary
+// TODO: use file to hold big dictionary or try random-word
+// console.log(randomWords());
+const dictionary = ['cat'];
 
-// Randomly pick word and create Word instance with it
+const Game = function(dictionary) {
+  this.dictionary = dictionary;
+  this.word = this.dictionary[0]; // TODO make this a random pick from dictionary
+  this.numGuesses = 5;
+  this.gameOver = false;
+  this.updateDisplay = function() {
+    // updates display after each letter has been guessed
+    // TODO
+  };
+  this.playGame = function() {
+    // main loop of the game
+    // RESUME: As written, inquirer prompts and then game goes into an infinite loop immediately
+    // comment out everything in .then and below except logging input and numGuesses
+    // if that is working, build out from there
+    const that = this;
+    if (!this.gameOver ) {
+      inquirer
+      .prompt([{
+        type: 'input',
+        message: 'Guess a letter please',
+        name: 'letter'
+      }])
+      .then(function(inquirerResponse) {
+        const letter = inquirerResponse.letter.trim();
+        // need to validate and normalize user input
+        // TODO: this is causing the game to loop infinitely, fix
+        // if (!letter) {
+        //   // ask again
+        // } else {
+          // TODO: build required functionality to replace this scaffolding
+          that.numGuesses--;
+          console.log(letter, that.numGuesses);
+          if (that.numGuesses === 0) {
+            console.log('Sorry, you have run out of guesses!');
+            that.gameOver = true
+          } else {
+            that.playGame(); // recursion
+          }
+//      };
+      });
+    }; // end of if loop
+  }; // end of playGame()
+} // end of Game
 
-// Prompt 'loop' starts
+const game = new Game(dictionary);
+game.playGame();
 
-// update display - or do it for the first time
 
-// prompt loop stops AFTER calling the start of the loop again
+// RESUME
+// build inquirer prompt, then show word and obscured word on screen, then get the loop going
+
+
 

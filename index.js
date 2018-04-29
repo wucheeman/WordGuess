cd6b870fa41462ffc7a9688ff310e5f8e2cce9ff
Mark Hainline <wucheeman@gmail.com>
2018-04-29 10:42:45 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// @ts-check
+// This turns on type checking in VS Code
+
+// require inpuirer
+// require Word
+
+// Create dictionary
+
+// Randomly pick word and create Word instance with it
+
+// Prompt 'loop' starts
+
+// update display - or do it for the first time
+
+// prompt loop stops AFTER calling the start of the loop again
+
